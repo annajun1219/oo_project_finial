dbc91827298d15138bd05056fd48163303c31d5b
annajun1219 <annajun@sookmyung.ac.kr>
2025-05-20 14:57:46 +0900

    [fetch] login

--- OO_Backend/src/main/java/com/example/oo_backend/user/entity/User.java
+++ OO_Backend/src/main/java/com/example/oo_backend/user/entity/User.java
@@ -1,10 +1,15 @@
 package com.example.oo_backend.user.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
 public class User {
 
     @Id
@@ -22,52 +27,5 @@ public class User {
 
     private LocalDate birth;
 
-    // Getters & Setters
-    public UUID getUserId() {
-        return userId;
-    }
-
-    public void setUserId(UUID userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public LocalDate getBirth() {
-        return birth;
-    }
-
-    public void setBirth(LocalDate birth) {
-        this.birth = birth;
-    }
+    private String profileImage;
 }
